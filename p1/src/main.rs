42db90ffe4b7de5a52058fa2428d448eb28bb755
Brian Billman <brian.billman@us.kbr.com>
2022-12-02 14:43:37 -0500

    prob1part1

--- p1/src/main.rs
+++ p1/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let tmp = include_str!("input1.txt");
+    let input_str: String = String::from(tmp);
+
+    let mut current: usize = 0;
+    let mut best: usize = 0;
+    for line in input_str.split('\n') {
+        if line.len() == 0 {
+            best = std::cmp::max(current, best);
+            current = 0;
+            continue;
+        }
+        current += match line.parse() {
+            Ok(val) => val,
+            Err(_) => 0,
+        };
+    }
+
+    println!("{}", best)
 }
